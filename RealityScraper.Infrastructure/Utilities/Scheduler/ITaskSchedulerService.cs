2e555ab9d9f15237b80c6cbd14a11f922537e195
Musil, Petr <info@petrmusil.net>
2025-05-25 16:45:40 +0200

    Extracted cronos as a service

---
+++ RealityScraper.Infrastructure/Utilities/Scheduler/ITaskSchedulerService.cs
@@ -0,0 +1,12 @@
+﻿using RealityScraper.Infrastructure.BackgroundServices.Scheduler;
+
+namespace RealityScraper.Infrastructure.Utilities.Scheduler;
+
+public interface ITaskSchedulerService
+{
+	Task<DateTime?> CalculateNextRunTimeAsync(string cronExpression, DateTime fromTime, CancellationToken cancellationToken);
+
+	Task<List<ScheduledTaskInfo>> LoadActiveTasksAsync(CancellationToken cancellationToken);
+
+	Task UpdateTaskExecutionTimesAsync(Guid taskId, DateTime lastRunTime, DateTime? nextRunTime, CancellationToken cancellationToken);
+}
